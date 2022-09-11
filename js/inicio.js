47b8ba8d19880a1d3e22b172e796e1e697e5fa4a
Jimxx22 <jaumegil22@gmail.com>
2022-09-11 19:45:40 +0200

    Inicio header, footer, sidemenu

---
+++ js/inicio.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.padding = "5vh";
+    document.getElementById("body").style.marginLeft="300px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.padding = "0";
+    document.getElementById("body").style.marginLeft="0px";
+}
