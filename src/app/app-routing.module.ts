81a296b3c60f673585910f5b74452cf28e7cc662
Jimxx22 <jaumegil22@gmail.com>
2022-09-16 20:57:24 +0200

    side menu funcitions and top menu routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameListComponent } from "./components/game-list/game-list.component";
+import { MyGamesComponent } from "./components/my-games/my-games.component";
+import { BorrowsListComponent } from "./components/borrows-list/borrows-list.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { GameDetailComponent } from "./components/game-detail/game-detail.component";
+import { AddGameComponent } from "./components/add-game/add-game.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: GameListComponent },
+  { path: 'mis-juegos', component: MyGamesComponent },
+  { path: 'mis-prestamos', component: BorrowsListComponent },
+  { path: 'perfil', component: ProfileComponent },
+  { path: 'juego', component: GameDetailComponent },
+  { path: 'juego-nuevo', component: AddGameComponent },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/my-games/my-games.component.ts
+++ src/app/components/my-games/my-games.component.ts
@@ -12,4 +12,20 @@ export class MyGamesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openNav() {
+    let mySidenav :HTMLElement = document.getElementById("mySidenav") !;
+    let body :HTMLElement = document.getElementById("body") !;
+    let subBar :HTMLElement = document.getElementById("sub-bar") !;
+    mySidenav.setAttribute( "style", "padding:10vh 2vh 2vh 2vh; width:250px;");
+    body.setAttribute( "style", "margin-left:300px;");
+    subBar.setAttribute( "style", "margin-left:250px;");
+  }
+  closeNav() {
+    let mySidenav :HTMLElement = document.getElementById("mySidenav") !;
+    let body :HTMLElement = document.getElementById("body") !;
+    let subBar :HTMLElement = document.getElementById("sub-bar") !;
+    mySidenav.setAttribute( "style", "padding:0vh; width:0px;");
+    body.setAttribute( "style", "margin-left:0px;");
+    subBar.setAttribute( "style", "margin-left:0px;");
+  }
 }
